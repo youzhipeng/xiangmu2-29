f7c96fa9571d65acae0efc3072cfa7a7e84cdb6b
youzhipeng <1220860524@qq.com>
2020-03-03 23:28:43 +0800

    第三天轮播 登入

--- gulpfile.js
+++ gulpfile.js
@@ -80,7 +80,7 @@ const serverHandler = () => {
              .pipe(webserver({ // 需要一些配置项
                host: 'localhost', // 域名, 这个域名可以自定义
                port: 8080, // 端口号, 0 ~ 65535, 尽量不适用 0 ~ 1023
-               open: './pages/login.html', // 你默认打开的首页, 从 dist 下面的目录开始书写
+               open: './pages/index.html', // 你默认打开的首页, 从 dist 下面的目录开始书写
                livereload: true, // 自动刷新浏览器 - 热重启
                // 所有的代理配置都在 proxies 里面y
                proxies: [
@@ -92,7 +92,7 @@ const serverHandler = () => {
                  },
                  {
                    source: '/gx2',
-                   target: 'http://127.0.0.1/xxx.php'
+                   target: 'http://localhost:80/DAY36/login.php'
                  }
                ]
              })) // 开启服务器
